da1b856d996a0d89721fe26603bd780f651ce2fc
dmh <dmytro.hrynevych@gmail.com>
2017-02-20 19:06:34 +0200

    update

--- main.js
+++ main.js
@@ -286,6 +286,28 @@ jQuery(function($) {
 
 })(jQuery);
 
+(function($) {
+    'use strict';
+
+    // document load event
+    $(document).ready(function() {
+        var $frame = $('.js__hero-image');
+        var $slider = $('.slider-container');
+        $frame.each(function() {
+            var self = $(this);
+            if (!self.parents('.swiper-wrapper').length){
+                self.addClass('_animated');
+            }
+        });
+        $slider.each(function() {
+            if ($(this).find($frame).length){
+                $(this).addClass('_full-width');
+            }
+        });
+    });
+
+})(jQuery);
+
 /* global Swiper*/
 (function($) {
     'use strict';
